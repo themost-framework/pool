a0c8a8529f6bcafb5f6b1d91ff7558a5d1739513
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2025-07-09 07:02:25 +0300

    use generic pool exporter

--- exporter/index.js
+++ exporter/index.js
@@ -1,9 +1,8 @@
-const {ApplicationService} = require('@themost/common');
+const {ApplicationService, TraceUtils} = require('@themost/common');
 // eslint-disable-next-line node/no-extraneous-require
 const {GenericPoolAdapter} = require('@themost/pool');
 const promClient = require('prom-client');
 const poolExporter = require('generic-pool-prometheus-exporter');
-
 
 class GenericPoolAdapterExporter extends ApplicationService {
     constructor(app) {
@@ -16,8 +15,12 @@ class GenericPoolAdapterExporter extends ApplicationService {
             const exporter = poolExporter(target);
             if (exporter) {
                 // register exporter with prom-client
-                promClient.register.registerMetric(exporter);
-            }
+                promClient.register.metrics().then(() => {
+                    TraceUtils.info(`Registered generic pool exporter for ${target.name}`);
+                }).catch((err) => {
+                    TraceUtils.error(`Failed to register generic pool exporter for ${target.name}:`, err);
+                });
+            }   
         });
     }
 
